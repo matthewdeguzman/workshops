816a3be0efe32721ef914c6695c496ef1c645e8c
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-26 21:03:33 -0400

    format files

--- colorstack/go-workshop/frontend/src/routes/+layout.server.ts
+++ colorstack/go-workshop/frontend/src/routes/+layout.server.ts
@@ -1,17 +1,15 @@
 import type { LayoutServerLoad } from './$types';
 import type { QrCode } from '$lib/types';
 
-
 export const load: LayoutServerLoad<{ codes: QrCode[] | null }> = async () => {
 	let codes = null;
 	try {
-		const resp = await fetch(`${import.meta.env.VITE_BASE_URL}/api/get?all=true`,
-			{
-				method: 'GET',
-				headers: {
-					Authorization: 'Bearer 1234567890'
-				}
-			});
+		const resp = await fetch(`${import.meta.env.VITE_BASE_URL}/api/get?all=true`, {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer 1234567890'
+			}
+		});
 
 		if (!resp.ok) {
 			throw new Error(resp.statusText);
@@ -23,5 +21,5 @@ export const load: LayoutServerLoad<{ codes: QrCode[] | null }> = async () => {
 	}
 	return {
 		codes
-	}
+	};
 };
--- colorstack/go-workshop/frontend/src/routes/qr/[id]/+page.server.ts
+++ colorstack/go-workshop/frontend/src/routes/qr/[id]/+page.server.ts
@@ -2,26 +2,28 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { QrCode } from '$lib/types';
 
-
-export const load: PageServerLoad<{ code: QrCode | null }> = async ({ params }: { params: { id: string } }) => {
+export const load: PageServerLoad<{ code: QrCode | null }> = async ({
+	params
+}: {
+	params: { id: string };
+}) => {
 	try {
-		const resp = await fetch(`${import.meta.env.VITE_BASE_URL}/api/get?id=${params.id}`,
-			{
-				method: 'GET',
-				headers: {
-					Authorization: 'Bearer 1234567890'
-				}
-			});
+		const resp = await fetch(`${import.meta.env.VITE_BASE_URL}/api/get?id=${params.id}`, {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer 1234567890'
+			}
+		});
 
 		if (!resp.ok) {
 			if (resp.status === 404) error(404, 'Uh oh! Could not find that code');
 			error(500, 'Uh oh! Something went wrong');
 		}
 
-		const code = await resp.json() as QrCode;
+		const code = (await resp.json()) as QrCode;
 		return {
 			code
-		}
+		};
 	} catch (err) {
 		error(err.status, err.body && err.body.message);
 	}
